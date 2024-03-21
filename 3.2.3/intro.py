b3fbe46a286b2bcd843a483924e15b7477cae735
The Cat <philip.birkland@vtsd.com>
2024-03-21 11:34:28 -0400

    3.2.3 step 10
    Philip Birkland

---
+++ 3.2.3/intro.py
@@ -0,0 +1,21 @@
+'''
+num_list = [42, 6, 11, 14, 7, 89, 17]
+
+for numbers in num_list:
+    print(numbers - 13)
+
+for loop in range(4):
+    print("cheese")
+'''
+
+for numbers in range(1, 20):
+    print(numbers)
+
+for numbers2 in range(20, 0, -1):
+    print(numbers2)
+
+for numbers3 in range(2, 11, 2):
+    print(numbers3)
+
+for numbers4 in range(9, 0, -2):
+    print(numbers4)
