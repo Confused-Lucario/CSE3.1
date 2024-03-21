ef1e3000b49310deca9f94e007138fbdd078196b
The Cat <philip.birkland@vtsd.com>
2024-03-21 11:35:28 -0400

    3.2.3 step 10
    Philip Birkland

--- 3.2.3/intro.py
+++ 3.2.3/intro.py
@@ -11,11 +11,17 @@ for loop in range(4):
 for numbers in range(1, 20):
     print(numbers)
 
+print("")
+
 for numbers2 in range(20, 0, -1):
     print(numbers2)
 
+print("")
+
 for numbers3 in range(2, 11, 2):
     print(numbers3)
+
+print("")
 
 for numbers4 in range(9, 0, -2):
     print(numbers4)
